0c393a6d7d03c1a121e9ef5d23780ab6054d0dae
skatrico <109701821+skatrico@users.noreply.github.com>
2025-05-11 11:12:58 +0200

    Add files via upload

---
+++ download organizer.ps1
@@ -0,0 +1,33 @@
+﻿$extensions = @{ 
+    "VIDEO" = @(".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv") 
+    "COMPRESSED_FILES" = @(".zip", ".rar", ".tar", ".gz", ".7z", ".bz2")
+    "EXECUTABLE_APPLICATIONS" = @(".exe", ".msi", ".bat", ".sh", ".apk")
+    "IMAGES" = @(".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp")
+    "TORRENTS" = @(".torrent", ".magnet")
+    "ISOs" = @(".iso", ".img", ".bin", ".cue", ".nrg", ".dmg", ".vhd", ".vmdk")
+    "3D" = @(".3mf", ".stl", ".gcode", ".obj", ".ply", ".fbx", ".dae", ".blend")
+    "OFFICE" = @(".docx", ".doc", ".xls", ".xlsx", ".ppt", ".pptx", ".ods", ".odp", ".csv")
+    "TEXT" = @(".txt", ".log", ".md", ".rst", ".json", ".xml", ".yaml", ".yml")
+
+}
+
+function order($extensions) {
+    $archives = Get-ChildItem -File -Path "$env:USERPROFILE\Downloads"
+
+    foreach ($key in $extensions.Keys) {
+        Write-Host -ForegroundColor Red "`nBuscando archivos de tipo: $key"
+
+        $destinationPath = "$env:USERPROFILE\Downloads\$key"
+        if (-not (Test-Path -Path $destinationPath)) {
+            New-Item -ItemType Directory -Path $destinationPath | Out-Null
+        }
+
+        foreach ($archive in $archives) {
+            if ($extensions[$key] -contains $archive.Extension) {
+                Move-Item -Path $archive.FullName -Destination $destinationPath
+            }
+        }
+    }
+}
+
+order $extensions
